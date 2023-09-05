00691207c8ac49af02b44470ba0977b7662c71d9
Jonatas Fernandes <128390310+jonatasfernandessilva7@users.noreply.github.com>
2023-09-05 09:50:37 -0300

    criando tasks com o simulador de kernel freeRTOS

---
+++ TasksFreeRTOS/src/main.c
@@ -0,0 +1,61 @@
+/* Standard includes. */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/* FreeRTOS kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+void vTask1(void *pvParameters);
+void vTask2(void *pvParameters);
+void vTask3(void *pvParameters);
+
+int main(void)
+{
+    xTaskCreate(&vTask1, /** tarefa ou função a ser executada */
+		"Task jonatas testando kernel",  /** nome da tarefa ou função */
+		 1024, /** tamanho da pilha  */
+		 NULL, /** parametro de entrada */ 
+		 1, /** prioridade da tarefa */ 
+		 NULL /** identificador da tarefa */
+		 );
+    xTaskCreate(&vTask2, "kernel foi", 1024, NULL, 2, NULL);
+    xTaskCreate(&vTask3, "teste finalizado", 1024, NULL, 3, NULL);
+
+    vTaskStartScheduler(); /** inicia o agendador de tarefas */
+
+    return 0;
+}
+
+void vTask1(void *pvParameters)
+{
+    for (;;)
+    {
+        printf("Task jonatas testando kernel\r\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void vTask2(void *pvParameters)
+{
+    for (;;)
+    {
+        printf("kernel foi\r\n");
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+}
+
+void vTask3(void *pvParameters)
+{
+	TaskHandle_t xHandle;
+	for (int i = 0;i < 1; i++)
+	{
+		printf("kernel finalizado\r\n");
+		vTaskDelay(pdMS_TO_TICKS(6000));
+		vTaskSuspend( xHandle );
+
+	}
+
+}
