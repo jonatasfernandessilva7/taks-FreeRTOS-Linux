980f660d59fee30b1345126007ccf72285e994ed
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-09-13 09:25:37 -0300

    voltando

--- TasksFreeRTOS/build/home/jonatas/freeRTOS_UFC/FreeRTOSv202212.01/FreeRTOS/Source/portable/ThirdParty/GCC/Posix/utils/wait_for_event.d
+++
@@ -1,3 +0,0 @@
-build//home/jonatas/freeRTOS_UFC/FreeRTOSv202212.01/FreeRTOS/Source/portable/ThirdParty/GCC/Posix/utils/wait_for_event.o: \
- /home/jonatas/freeRTOS_UFC/FreeRTOSv202212.01/FreeRTOS/Source/portable/ThirdParty/GCC/Posix/utils/wait_for_event.c \
- /home/jonatas/freeRTOS_UFC/FreeRTOSv202212.01/FreeRTOS/Source/portable/ThirdParty/GCC/Posix/utils/wait_for_event.h
